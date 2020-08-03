bcf6f2eb03466c5c6e9ea466adeeec6fcdc17428
kevinjia <kevinjia1984@gmail.com>
2020-08-03 13:37:06 +1000

    update the secretkey

--- week06/MovieShorts/MovieShorts/settings.py
+++ week06/MovieShorts/MovieShorts/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'k*(^1-k_#m2a0(sy4k*j0_@41=9my4oox76*+^jt^a%mf2uid$'
+SECRET_KEY = 'very secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
